484e0705cc9206c316d57a3d44a39bcc753fe5a6
神馬 健介 <kensuke@shinmenoMacBook-Pro.local>
2020-06-30 00:38:48 +0900

    how to v-for

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 new Vue({
     el:'#app',
     data:{
-        message:'Hello Vue.js!'
+        message:'Hello Vue.js!',
+        count: 10,
+        user:{
+            lastName:'Yamada',
+            firstName:'Taro',
+            prefecture:'Tokyo',
+            age:'28'
+        },
+        colors:['Red','Green','Blue'],
+        toggle:false
+
     }
 })
