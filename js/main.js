67e62f411443513b2f711b5b97d0d8d4e17ffdd3
神馬 健介 <kensuke@shinmenoMacBook-Pro.local>
2020-06-30 22:56:46 +0900

    GlobalFilter

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
+Vue.filter('numberFormat',function(value){
+    return value.toLocaleString()
+})
+
 var app = new Vue({
     el: '#app',
     data:{
         price:29800
     },
-    filters:{
-        numberFormat:function(value){
-            return value.toLocaleString()
-        }
-    }
+    // filters:{
+    //     numberFormat:function(value){
+    //         return value.toLocaleString()
+    //     }
+    // }
 
 })
