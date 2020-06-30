be39377fdced0fc3f2e37d0e121a5011bf6163a1
神馬 健介 <kensuke@shinmenoMacBook-Pro.local>
2020-06-30 09:08:44 +0900

    how to v-model

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
-new Vue({
+var app = new Vue({
     el:'#app',
     data:{
-        message:'Hello Vue.js!',
-        count: 10,
-        user:{
-            lastName:'Yamada',
-            firstName:'Taro',
-            prefecture:'Tokyo',
-            age:'28'
-        },
-        colors:['Red','Green','Blue'],
-        toggle:false,
-        now:''
-    },
-    methods:{
-        onclick: function(){
-            //alert('onclick');
-            this.now = new Date().toLocaleString();
-        }
+        message:'Hello Vue.js!'
     }
 })
