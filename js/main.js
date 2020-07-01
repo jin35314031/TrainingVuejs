7efc3f2795e4ee749bbdf39d7174127c9891e4a3
神馬 健介 <kensuke@shinmenoMacBook-Pro.local>
2020-07-01 13:38:13 +0900

    Get data from API

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 var app = new Vue({
     el: '#app',
     data:{
+        items:null,
+        keyword:'',
+        message:''
+    },
+    watch:{
         
     },
+    created:function(){
+        this.keyword = 'JavaScript'
+        this.getAnswer()
+    },
     methods:{
-        
+        getAnswer:function(){
+            if(this.keyword == ''){
+                this.item = null
+                return
+            }
+
+            this.message = 'Loading...'
+            var vm = this
+            var params = { page:1 ,per_page: 20,query: this.keyword }
+            axios.get('https://qiita.com/api/v2/items',{params})
+            .then(function(response){
+                console.log(response)
+                vm.items = reponse.data
+            })
+            .catch(function(error){
+                vm.message = 'Error!' + error
+            })
+            .finally(function(){
+                vm.message = ''
+            })
+        }
     }
 })
