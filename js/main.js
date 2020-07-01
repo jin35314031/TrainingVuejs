9d853d8a39a7d2c9e0162a8192cd13b9d192a681
神馬 健介 <kensuke@shinmenoMacBook-Pro.local>
2020-07-01 15:42:52 +0900

    array syntax v-bind

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var app = new Vue({
     el: '#app',
     data:{
-        isLarge:true,
-        hasError:false
+        largeClass:'large',
+        dangerClass:'text-danger'
     },
     methods:{
         
