50b5c10017c317db668ecc1e85d9dd8d6a8a0bb5
神馬 健介 <kensuke@shinmenoMacBook-Pro.local>
2020-07-01 12:43:31 +0900

    option deep1

--- js/main.js
+++ js/main.js
@@ -1,29 +1,19 @@
-// var app = new Vue({
-//     el: '#app',
-//     data:{
-//         firstName:'',
-//         lastName:'',
-//         fullName:''
-//     },
-//     watch:{
-//         firstName:function(value){
-//             this.fullName = value +' '+ this.lastName
-//         },
-//         lastName:function(value){
-//             this.fullName = this.firstName +' '+value 
-//         }
-//     }
-// })
-
 var app = new Vue({
     el: '#app',
     data:{
-        firstName:'',
-        lastName:''
+        colors:[
+            {name:'Red'},
+            {name:'Green'},
+            {name:'Blue'}            
+        ]
     },
-    computed:{
-        fullName:function(){
-            return this.firstName +' '+ this.lastName
+    watch:{
+        colors:{
+            handler:function(newValue,oldValue){
+                console.log('Update')
+            },
+            deep:true
         }
     }
 })
+
