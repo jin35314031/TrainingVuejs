380018684f80b528629f110ff34afaf0ee4914f7
神馬 健介 <kensuke@shinmenoMacBook-Pro.local>
2020-07-01 12:34:38 +0900

    computed fullName

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
+// var app = new Vue({
+//     el: '#app',
+//     data:{
+//         firstName:'',
+//         lastName:'',
+//         fullName:''
+//     },
+//     watch:{
+//         firstName:function(value){
+//             this.fullName = value +' '+ this.lastName
+//         },
+//         lastName:function(value){
+//             this.fullName = this.firstName +' '+value 
+//         }
+//     }
+// })
+
 var app = new Vue({
     el: '#app',
     data:{
         firstName:'',
-        lastName:'',
-        fullName:''
+        lastName:''
     },
-    watch:{
-        firstName:function(value){
-            this.fullName = value +' '+ this.lastName
-        },
-        lastName:function(value){
-            this.fullName = this.firstName +' '+value 
+    computed:{
+        fullName:function(){
+            return this.firstName +' '+ this.lastName
         }
     }
 })
