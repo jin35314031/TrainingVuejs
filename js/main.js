e881b9d929cbe4c3da6656775f8df0eb7b983c61
神馬 健介 <kensuke@shinmenoMacBook-Pro.local>
2020-07-07 15:41:22 +0900

    localRegistration

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-Vue.component('hello-component',{
-    template: '<p>Hello</p>'
-})
+var helloComponent = {
+    template:'<p>Hello</p>'
+}
 
 var app = new Vue({
     el: '#app',
-    data:{
-        
-    },
-    methods:{
-        
+    components:{
+        'hello-component': helloComponent
     }
 })
