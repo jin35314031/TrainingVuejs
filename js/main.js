7b5d73769e23ff2adfe60efa83e151be27e64171
神馬 健介 <kensuke@shinmenoMacBook-Pro.local>
2020-07-07 16:05:50 +0900

    componentOption

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-var helloComponent = {
-    template:'<p>Hello</p>'
-}
-
+Vue.component('button-counter',{
+    // 関数で定義
+    data:function(){
+        return{
+            count:0
+        }
+    },
+    // 要素は単一、もしくはdivなどで囲む必要がある
+    //template:'<button v-on:click="count++">{{ count }}</button>'
+    template:'<div> <span>count: </span> <button v-on:click="count++"> {{count}} </button> </div>'
+})
 var app = new Vue({
-    el: '#app',
-    components:{
-        'hello-component': helloComponent
-    }
+    el: '#app'
+    
 })
